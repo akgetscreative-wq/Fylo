a26a0ea26c16ce7dddf6ce2ada9cdee6ffd2f6bb
AayushTheBoss <akgetscreative@gmail.com>
2026-09-25 14:30:16 +0530

    fix(mobile): resolve ReferenceError Property 'console' doesn't exist by configuring @react-native/metro-config polyfills and defensive global console

--- mobile/index.js
+++ mobile/index.js
@@ -1,3 +1,18 @@
+// Defensive initialization to guarantee console exists in all JS engines
+if (typeof global !== 'undefined') {
+  if (typeof global.console === 'undefined') {
+    const noop = () => {};
+    global.console = {
+      log: noop,
+      info: noop,
+      warn: noop,
+      error: noop,
+      debug: noop,
+      trace: noop,
+    };
+  }
+}
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
